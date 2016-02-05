b34e89f15d3671f053b8dad1e869201cfbda88c6
Sean Lewis <sensaisean@gmail.com>
2016-02-05 00:44:20 -0700

    download x pages from any subreddit

--- lib/redd.ex
+++ lib/redd.ex
@@ -2,8 +2,10 @@ defmodule Redd do
 
   @directory Crawl.dl_dir_check
 
-  def subreddit_get(url) do
-    subreddit = to_string(Regex.scan(~r/r\/(.*)/, url, capture: :all_but_first))
+  def subreddit_get(url, page_count \\ 1) do
+    subreddit = Regex.scan(~r/r\/(.*)\//, url, capture: :all_but_first)
+    |> hd
+    |> to_string
     if subreddit == "" do
       raise "URL: #{url} is invalid"
     end
@@ -12,12 +14,20 @@ defmodule Redd do
 
     File.mkdir(path)
 
-    imgur_list = Crawl.get(url)
-      |> Crawl.find("p.title > a")
+    page = Crawl.get(url)
+
+    imgur_list = Crawl.find(page, "p.title > a")
       |> Enum.map(fn(x) -> Floki.attribute(x, "href") end)
       |> List.flatten
       |> Imgur.domain_filter
-    process_list = Enum.map(imgur_list, fn(x) -> spawn fn -> Imgur.get_images(x, path) end end )
-    Enum.map(process_list, fn(x) -> Crawl.wait_for_process(x) end)
+    next_link = Floki.find(page, "span.nextprev > a")
+      |> Floki.attribute("href")
+      |> hd
+    if page_count > 0 do
+      page_count = page_count - 1
+      subreddit_get(next_link, page_count)
+      process_list = Enum.map(imgur_list, fn(x) -> spawn fn -> Imgur.get_images(x, path) end end )
+      Enum.map(process_list, fn(x) -> Crawl.wait_for_process(x) end)
+    end
   end
 end
